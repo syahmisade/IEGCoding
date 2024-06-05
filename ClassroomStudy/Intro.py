73a8bc1a7ac8b6b85ce1cf0675ed786c0e7374e0
Syahmi Sade <syhmiisade@gmail.com>
2024-06-05 16:08:52 +0800

    Adding contents and edit folder name

---
+++ ClassroomStudy/Intro.py
@@ -0,0 +1,43 @@
+x = 4
+y = 4
+
+print(x+y)
+
+# Penghadang supaya tak confius maa
+print(" ")
+print("#######################################################")
+print(" ")
+
+# From Mr.Jegan ##################################################################
+# print => python build in function
+# will get help from windows to print the output in the monitor (output device)
+# when you call a function, must be followed by ()
+# in (), we will have n numbers of arguments
+###################################################################################
+
+print("Welcome to python guys!!!") # 1 function, 1 arguments
+print("Nasi lemak","Teh ais") # 1 function, 2 arguments
+
+print(" ")
+print("#######################################################")
+print(" ")
+
+# function => arahan yang dipendekkan, tak payah cakap panjang2, cakap simple2 jer
+# Contoh => Belikan makanan, tak perlu cakap tapau kan elok2 letak dalam bekas bagai
+# argument => apa yang nak difocuskan based on the function
+# Contoh (daripada contoh tadi) => Nasi goreng ayam dengan teh ais ikat tocang (2 arguments)
+
+# From Mr.Jegan ##################################################################
+# product is a variable
+# Television is the string value/literal
+# To differentiate the variables from values => "" OR ''
+#################################################################################
+
+product = "Television" # string
+quantity = 2 # integer
+price = 1000.50 # float
+available = True # booleanvalue/literal
+print("Product:",product)
+print("Quantity:",quantity)
+print("Price:",price)
+print("Availability:",available)

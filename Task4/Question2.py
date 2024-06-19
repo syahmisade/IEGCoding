bc3700df67ca45a89c84cff3e941d42d2c1e197f
Syahmi Sade <syhmiisade@gmail.com>
2024-06-19 10:56:02 +0800

    Updating Q2 in Task4

--- Task4/Question2.py
+++ Task4/Question2.py
@@ -17,11 +17,10 @@ for num in nums:
 '''
 nums = range(1, 31)
 primeNum = []
-is_prime = True
 
 for num in nums:
     if num > 1:
-        is_prime
+        is_prime = True
         for i in range(2, int(num**0.5) + 1):
             if num % i != 0:
                 is_prime = True

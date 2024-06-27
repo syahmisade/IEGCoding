610071d6aa57ab0e6773789f43669e9f85f2ecca
Syahmi Sade <syhmiisade@gmail.com>
2024-06-28 02:58:26 +0800

    Creating try4.py

---
+++ Conteng/try4.py
@@ -0,0 +1,31 @@
+def minus(a,b):
+    finalminus = a - b
+    return finalminus
+
+def multiply():
+    a = minus(1,2)
+    b = 2
+    jawapan = a*b
+    return print(jawapan)
+
+# x = int(input())  
+
+minus(1,4)
+
+# a = 1*2
+# b = 2*2
+# c = 3*2
+
+# for i in range(1,4):
+#     print(i*2)
+
+burger = 5
+while burger>0: # selagi
+    burger = burger - 1
+    print(f"{burger} burger")
+    
+
+# int()
+# input()
+# minus()
+

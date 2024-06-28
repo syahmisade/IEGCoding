a8211d231412b9c4b5e4f1a9d3224c56fd993095
Syahmi Sade <syhmiisade@gmail.com>
2024-06-28 12:28:50 +0800

    Added new contain for ml.py and myClass.py

---
+++ June28/ml.py
@@ -0,0 +1,11 @@
+'''
+Lectured by Mr.Jegan
+
+Object Oriented Programming (OOP):
+1. Class
+2. Object
+3. Inheritance
+4. Polymorphism
+5. Encapsulation
+
+'''
---
+++ June28/myClass.py
@@ -0,0 +1,59 @@
+'''
+Class:
+
+- Possible to become different role based on the event
+- Assign variables => creating a Class attributes
+- def __init__() => a function : can be used into coding
+    - self => a variable : can be used into coding (used as parameter in __init__())
+
+'''
+class Ultraman:
+    planet = "Nebula M60" # one of the Class attributes
+    
+    # this is an initializer (Instance Initialization) / Instance Attributes
+    def __init__(self,name,power,warna): # Taking parameter
+        self.planet = "Nebula M80" # one of the Instace Attributes (Example of without taking a parameter)
+        self.name = name
+        self.power = power
+        self.__warna = warna
+        self.listnama = "" # Initialize it as a blank so that we can submit something
+    
+    # This is a method
+    def kuasa(self):
+        statement = f"Kuasa {self.name} : {self.power}"
+        return statement
+
+    def getWarna(self):
+        statement = f"Warna {self.name} : {self.__warna}"
+        return statement
+    
+    def setWarna(self,warna):
+        self.__warna = warna
+    
+    def __str__(self):
+        statement = f"{self.name} is from {self.planet}"
+        return statement
+
+# --- Outside Class -----------------------------------------------------------------------------------------------------
+    
+# Calling Class including the instances:  how many Arguments (== "Ultraman Gaia",1000) will be depending on the Class's Initializer
+ultraman = Ultraman("Ultraman Gaia",500,"Merah Putih Emas Hitam")
+name = ultraman.name # Signing Ultraman instance attributes
+kuasa = ultraman.kuasa() # Calling a method from Ultraman Class
+warna = ultraman.getWarna()
+
+ultraman.setWarna("Biru Hitam Emas")
+warnaBaru = ultraman.getWarna()
+
+# Both printed different thing because both of the meaning is different
+print(ultraman.planet) # Based from the init(self) function => Instance Attributes
+print(Ultraman.planet) # Based directly from Class => Class Attributes
+print(name)
+print(kuasa)
+print(warna)
+print(warnaBaru)
+print(ultraman)
+
+# type
+print(type(Ultraman("Ultraman Gaia",500,"Merah Putih Emas Hitam")))
+print(type(int("12")))

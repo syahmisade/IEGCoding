0053da3ca3a19337174bb2fe81e59d56f8b0a515
Syahmi Sade <syhmiisade@gmail.com>
2024-07-05 01:53:16 +0800

    Q7 in Task7

--- Task7/Question7.py
+++ Task7/Question7.py
@@ -1,5 +1,5 @@
 '''
-Write a Python class Employee with properties id, name, salary, and department and methods like _init_ calculateSalary, assignDepartment and 
+Write a Python class Employee with properties id, name, salary, and department and methods like _init_, calculateSalary, assignDepartment and 
 _str_.
 Sample Employee Data:
 "E7876", "ADAMS", 50000, "ACCOUNTING"
@@ -10,5 +10,41 @@ Use 'assignDepartment' method to change the department of an employee.
 Use '_str_' method to print the details of an employee.
 Use 'calculateSalary' method takes two arguments: salary and hours_worked, which is the number of hours worked by the employee. 
 If the number of hours worked is more than 50, the method computes overtime and adds it to the salary.
-Overtime is calculated as following formula: overtime = hours_worked - 50 Overtime amount = (overtime * (salary / 50))
+Overtime is calculated as following formula: 
+overtime = hours_worked - 50 Overtime 
+amount = (overtime * (salary / 50))
 '''
+class Employee:
+    def __init__(self, id, name, salary, department):
+        self.id = id
+        self.name = name
+        self.salary = salary
+        self.department = department
+    
+    def assignDepartment(self, department):
+        self.department = department
+    
+    def calculateSalary(self,hoursWorked):
+        overtime = 0
+        if hoursWorked > 50:
+            overtime = hoursWorked - 50
+        amount = overtime * (self.salary / 50)
+        self.salary = self.salary + amount
+        return self.salary
+    
+    def __str__(self):
+        statement = f"Worker's Details: \n"
+        statement += f"ID: {self.id}\n"
+        statement += f"Name: {self.name}\n"
+        statement += f"Salary: RM{self.salary:.2f}\n"
+        statement += f"Department: {self.department}"
+        return statement
+
+worker1 = Employee("E7876", "ADAMS", 50000, "ACCOUNTING")
+
+worker1.assignDepartment("SALES")
+worker1.calculateSalary(100)
+
+# Change from "ACCOUNTING" to "SALES"
+# Update the salary including the overtime
+print(worker1) 
---
+++ Task7/Question8.py
@@ -0,0 +1,19 @@
+'''
+Write a Python class Inventory with attributes like id, productName, availableQuantity and price. 
+Add methods like addItem, updateItem, and checkItem_details.
+Use a dictionary to store the item details, where the key is the id and the value is a dictionary containing the productName, 
+availableQuantity and price.
+Sample Data:
+{
+  "97410": {
+    "name": "Television",
+    "availableQuantity": 20,
+    "price": 1455.99
+  },
+  "97411": {
+    "name": "Radio",
+    "availableQuantity": 32,
+    "price": 654.25
+  }
+}
+'''

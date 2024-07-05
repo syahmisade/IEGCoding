10b65d4d2cfed031b5d53e451a919c54fd9e26d3
Syahmi Sade <syhmiisade@gmail.com>
2024-07-05 17:50:11 +0800

    Updating files

--- Conteng/try7.py
+++ Conteng/try7.py
@@ -7,14 +7,74 @@ contoh = "awdsdaef dawdsdfa dawdasda "
 # xstr = str(x)
 # print(xstr)
 
-example = "what is 5?".split()
-x,y,z = "what","is","?"
-contoh = list(example)
-if x and y and z in contoh:
-    contoh.remove(x)
-    contoh.remove(y)
-    contoh.remove(z)
-else:
-    print("This machine does not understand the statement")
+# example = "what is 5?".split()
+# x,y,z = "what","is","?"
+# contoh = list(example)
+# if x and y and z in contoh:
+#     contoh.remove(x)
+#     contoh.remove(y)
+#     contoh.remove(z)
+# else:
+#     print("This machine does not understand the statement")
 
-print(contoh)
+# print(contoh)
+
+# contoh 1
+def case_1():
+    return "Case 1"
+
+def case_2():
+    return "Case 2"
+
+def case_default():
+    return "Default case"
+
+dispatch = {
+    1: case_1,
+    2: case_2
+}
+
+x = int(input("Enter a number to determine the case: "))
+result = dispatch.get(x, case_default)()
+print(result)
+
+# Ternary conditional operator 2
+result = "Greater" if x > 5 else "Smaller or Equal"
+print(result)
+
+# Other ways 3
+def condition_1():
+    return "Condition 1"
+
+def condition_2():
+    return "Condition 2"
+
+def default():
+    return "Default"
+
+conditions = {
+    True: condition_1,
+    False: condition_2
+}
+
+x = 10
+result = conditions.get(x > 5, default)()
+print(result)
+
+# Other ways 4
+x = 10
+result = (x > 5 and "Greater") or "Smaller or Equal"
+print(result)
+
+# Other ways 5
+numbers = [1, 2, 3, 4, 5]
+result = ["Even" if num % 2 == 0 else "Odd" for num in numbers]
+print(result)
+
+# Other ways 5
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    result = "Cannot divide by zero"
+
+
--- webproject/flasktutor/app.py
+++ webproject/flasktutor/app.py
@@ -1,10 +1,11 @@
 from flask import Flask # type: ignore
 
-# http://127.0.0.1:5000 # 404 Error (Not Found)
+# http://127.0.0.1:5000 # kalau tak run server -> 404 Error (Not Found)
+# flask run -h 172.16.0.101 -> sir jegan punya
 
 # print("__main__")
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # a decorator
 def say():
     return "Hello Sahabat Organik!"

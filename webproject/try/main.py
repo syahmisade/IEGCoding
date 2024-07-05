016edb7a4d87da4196b55cc55608cb3a2a5d9a88
Syahmi Sade <syhmiisade@gmail.com>
2024-07-05 11:23:17 +0800

    Adding example of flask tutorial (could be deleted)

---
+++ webproject/try/main.py
@@ -0,0 +1,15 @@
+'''
+Web Python Project
+1. Adding Numpy 
+    res => p.i.p.y res
+    tool => p.i.p
+2. Create flask tutorial: python -m venv virtual
+    activate venv: 
+        1. Bawak pegi directory "activate.bat"
+        2. tekan "activate"
+        3. (virtual) tengah hidup
+3. Install semua dalam virtual scrip folder
+    - Gerakkan directory ke flasktutor balik
+    - => pip install flask
+4. Dah boleh "deactivate" tapi kena deactivate dekat folder Scripts
+''' 

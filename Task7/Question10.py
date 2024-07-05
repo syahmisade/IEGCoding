896e8364076f97256b63e283ea33795a7977be61
Syahmi Sade <syhmiisade@gmail.com>
2024-07-05 17:57:44 +0800

    Q9 and Q10 in Task7

---
+++ Task7/Question10.py
@@ -0,0 +1,9 @@
+'''
+Write a Python class to check the validity of a string of parentheses,
+
+'(', ')', '{', '}', '[' and '].
+These brackets must be closed in the correct order, for example
+
+"()" and "()[]{}" are valid
+"[)", "({[)]" and "{{{" are invalid.
+'''
---
+++ Task7/Question9.py
@@ -0,0 +1,4 @@
+'''
+Write a Python class BankAccount with attributes like accountNumber, openingBalance, currentBalance dateOfOpening and 
+customerName. Add methods like deposit, withdraw, and checkBalance.
+'''

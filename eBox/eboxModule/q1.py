bb6b4825e566a9595787a9d8017f7e7411e85358
Syahmi <amuhamadsade@sheffield.ac.uk>
2024-07-07 17:14:43 +0800

    Updated folder and q1 files in eboxModule

---
+++ eBox/eboxModule/q1.py
@@ -0,0 +1,19 @@
+'''
+The teacher was teaching about number series generation and one of the series is 
+like she will write first two number of the series and each student has to write a next number of
+the series on the board and 
+next number will be the addition of previous two numbers and first two numbers will be 0 and 1. 
+So, help the students by writing a program to generate the Fibonacci series.
+Input Format:  
+The input consists of integer which represents 'n' as number of values in the series
+Output Format:
+The output consists of series of integer numbers.
+Sample Input 1:
+3
+Sample Output1:
+0 1 1
+Sample Input 2:
+6
+Sample Output2:
+0 1 1 2 3 5
+'''

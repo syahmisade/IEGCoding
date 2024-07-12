4a88a87409e104c8bba25040c4325d43307700f8
Syahmi Sade <syhmiisade@gmail.com>
2024-07-12 17:12:20 +0800

    Adding updates for todays documents

---
+++ July12/ml.py
@@ -0,0 +1,29 @@
+'''
+Exploratory Data Analysis (EDA)
+- Raw data
+    - Know how extract data
+    - regular expression
+- Structure Data
+    - DB/RDBMS/NOSQL
+    - From Machines -> CSV/Excel
+- Data Preparation (Pandas)
+- Feature Engineerings (Pandas)
+    - Many tecniques
+- EDA comes in:
+    - Correlation formulas
+    - Covariances formulas
+    - Graphs -> to display the data
+
+data.gov
+
+- Have an idea to gain a result from a data
+- Study the company objective
+    - Based on the company data. For example, sales, we need to propose idea to make sure the it improved based on their obj
+- Data Science -> too many hype, but no vancancies
+    - AI
+    - Machine Learning
+    - Data Science
+- SME
+    - Small and Mid Industries
+    - Ecommence system
+'''
